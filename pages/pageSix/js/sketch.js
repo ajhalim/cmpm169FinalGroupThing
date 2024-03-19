8b31cc4b2b193cdedff914ab52e7f82272504335
Arthur Lin <arslin@ucsc.edu>
2024-03-19 14:13:58 -0700

    page6 added

---
+++ pages/pageSix/js/sketch.js
@@ -0,0 +1,38 @@
+// Author: Arthur Lin
+let lines = [];
+let numLines = 300;
+let lineHeight = 30;
+let wordBank = ["0", "1"];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  textSize(16);
+  for (let i = 0; i < numLines; i++) {
+    lines.push(generateLine());
+  }
+}
+
+function draw() {
+  background(0); // Black background
+  for (let i = 0; i < lines.length; i++) {
+    fill(0, 255, 0); // Green color
+    text(lines[i].text, lines[i].x, lines[i].y);
+    lines[i].y += lines[i].speed;
+    if (lines[i].y > height) {
+      lines[i] = generateLine();
+    }
+  }
+}
+
+function generateLine() {
+  let textContent = "";
+  let numWords = int(random(1, 5));
+  let x = random(width);
+  let y = random(-height, 0);
+  let speed = random(1, 3);
+  for (let i = 0; i < numWords; i++) {
+    let word = random(wordBank);
+    textContent += word + " ";
+  }
+  return { text: textContent, x: x, y: y, speed: speed };
+}
